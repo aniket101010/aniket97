7f63272048dadbcefe3d00a7d478435600b96402
aniket <aniket@Aniket.localdomain>
2024-06-18 22:06:35 +0530

    varbl1

---
+++ varbal.tf
@@ -0,0 +1,22 @@
+variable "os-name" {
+  default = "ami-0e1d06225679bc1c5"
+
+
+}
+variable "intance-type" {
+  default = "t2.micro"
+
+}
+variable "vpc-cdir" {
+    default =  "10.0.0.0/16"
+  
+}
+variable "subnet1-cdir" {
+    default = "10.0.1.0/24"
+  
+}
+
+
+
+
+
